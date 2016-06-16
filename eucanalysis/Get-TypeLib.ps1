1a2e84dd6b6b773646b2af6bd7f3db3bd2a1d92f
Paul Greer <paul.greer@redpixie.com>
2016-06-16 14:32:02 +0100

    Added GUID to output

--- eucanalysis/Get-TypeLib.ps1
+++ eucanalysis/Get-TypeLib.ps1
@@ -1,6 +1,8 @@
 ﻿<#
   .SYNOPSIS
   Accepts pipeline of TypeLib Guid Strings and looks them up in the registry and then checks existence of file 
+  Useful if you have a load of broken links and can run this script on a machine where the files exist.
+  Helps to identify them
 
   .PARAMETER input
   Pipeline Strings with GUID string 
@@ -51,15 +53,21 @@ function Get-TypeLib {
         }
 
         $paths = [com.redpixie.euc.mcdf.PsWrapper]::GetLibraryForTypeLibGuid($guid)
-        foreach ($path in $paths){
+
+        if ( $paths.Count -gt 0 ){
+            $path = $paths[0]
             if ( test-Path $path ){
-                $result.Add(@{path=$path; IsBroken=$false})
+                [void]$result.Add(@{guid=$guid;path=$path; IsBroken=$false})
             }
             else {
-                $result.Add(@{path=$path; IsBroken=$true})
+                # This cant really happen!
+                [void]$result.Add(@{guid=$guid;path=$path; IsBroken=$true})
             }
         }
-        
+        else {
+            [void]$result.Add(@{guid=$guid; path=""; IsBroken=$true})
+        }
+
         $result
     }
 
